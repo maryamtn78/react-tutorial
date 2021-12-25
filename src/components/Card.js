19dd7396d9360849bb12004652825dfe8c2dd444
Maryam Tohidiyan <m.tohidiyan78@gmail.com>
2021-12-24 23:24:22 -0800

    some changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,11 +3,11 @@ import styles from "./Card.module.css"
 
 class Card extends Component {
     render() {
-        const {image , name , price} = this.props
+        const {image , name , price} = this.props;
         return (
             <div className={styles.eachCard}>
                 <img src={image} alt='cardimg'/>
-                <h1>{name}</h1>
+                <h2>{name}</h2>
                 <p>{price}</p>
             </div>
         );
